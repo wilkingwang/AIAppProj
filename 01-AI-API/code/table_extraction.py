1e6215e993e783e19eecbc74fb073f49a3dcda5f
wilkingwang <wilkingwang@163.com>
2025-07-23 01:25:47 +0800

    01作业

---
+++ 01-AI-API/code/table_extraction.py
@@ -0,0 +1,46 @@
+import json
+import os
+import base64
+import dashscope
+from dashscope.api_entities.dashscope_response import Role
+
+dashscope.api_key = "sk-f85d4acdec0b42b2a46fc87ee877a19c"
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+base64_image = encode_image("./pdf_table.jpg")
+
+def llm_call(messages):
+    response = dashscope.MultiModalConversation.call(
+        model='qwen-vl-plus',
+        messages=messages
+    )
+
+    return response
+
+messages = [
+    {
+        "role": "system",
+         "content": [
+            {
+                 "text": "You are a helpful assistant."
+            }
+        ]
+    },
+    {
+        'role': 'user',
+        'content': [
+            {
+                'image': f"data:image/jpeg;base64,{base64_image}"
+            },
+            {
+                'text': '这是一个表格图片，帮我提取里面的内容，输出JSON格式'
+            }
+        ]
+    }
+]
+
+response = llm_call(messages)
+print(response)
---
+++ 01-AI-API/code/weather_function_call.py
@@ -0,0 +1,125 @@
+import json
+import os
+import dashscope
+from dashscope.api_entities.dashscope_response import Role
+
+dashscope.api_key = "sk-f85d4acdec0b42b2a46fc87ee877a19c"
+
+def get_current_weather(location, unit="摄氏度"):
+    temperature = -1
+
+    if '大连' in location or 'Dalian' in location:
+        temperature = 10
+    
+    if '上海' in location or 'Shanghai' in location:
+        temperature = 10
+
+    if '深圳' in location or 'Shenzhen' in location:
+        temperature = 10
+    
+    weather_info = {
+        "location": location,
+        "temperature": temperature,
+        "unit": unit,
+        "forecast": ["晴天", "微风"]
+    }
+
+    return json.dumps(weather_info)
+
+def llm_call(messages):
+    try:
+        response = dashscope.Generation.call(
+            model='qwen-max',
+            messages=messages,
+            functions=functions ,
+            result_format='message'
+        )
+
+        return response
+    except Exception as ex:
+        print(f"LLM API调用失败：{str(ex)}")
+        return None
+
+def run_conversation(prompt):
+    messages = [
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+
+    response = llm_call(messages)
+    if not response or not response.output:
+        print("获取LLM响应失败")
+        return None
+    
+    print(response)
+
+    message = response.output.choices[0].message
+    messages.append(message)
+    print('message=', message)
+
+    if hasattr(message, 'function_call') and message.function_call:
+        function_call = message.function_call
+        tool_name = function_call['name']
+
+        arguments = json.loads(function_call['arguments'])
+        print('arguments=', arguments)
+
+        tool_response = get_current_weather(
+            location=arguments.get('location'),
+            unit=arguments.get('unit')
+        )
+
+        tool_info = {
+            "role": "function",
+            "name": tool_name,
+            "content": tool_response
+        }
+        print('tool_info=', tool_info)
+        messages.append(tool_info)
+        print('messages=', messages)
+
+        # step4: 得到第二次响应
+        response = llm_call(messages)
+        if not response or not response.output:
+            print("获取LLM第二次响应失败")
+            return None
+        
+        print('response=', response)
+        message = response.output.choices[0].message
+        return message
+    
+functions = [
+    {
+        'name': 'get_current_weather',
+        'description': 'Get the current weather in a given location.',
+        'parameters': {
+            'type': 'object',
+            'properties': {
+                'location': {
+                    'type': 'string',
+                    'description': 'The city and state, e.g. San Francisco, CA'
+                },
+                'unit': {
+                    'type': 'string',
+                    'enum': [
+                        'celsius',
+                        'fahrenheit'
+                    ]
+                }
+            },
+            'required': ['location']
+        }
+    }
+]
+
+def main():
+    result = run_conversation('大连的天气怎样')
+    if result:
+        print('最终结果: ', result)
+    else:
+        print('对话执行失败')
+
+if __name__ == '__main__':
+    main()
